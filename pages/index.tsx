bbe1ae4c1da2d573e5144c3ab06e3c767f0af774
jonathasborges1 <jbc@icomp.ufam.edu.br>
2022-01-07 00:30:37 -0400

    v0.0.1

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const index = () => {
+  return(
+    <div>
+      <h1 className="bg-red-900"> Hello Word </h1>
+    </div>
+  )
+}
+
+export default index
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [ './pages/**/*.tsx', './components/**/*.tsx', ],
+  darkmode: false,
+  theme: {
+    extend: {},
+  },
+  variants:[],
+  plugins: [],
+}
